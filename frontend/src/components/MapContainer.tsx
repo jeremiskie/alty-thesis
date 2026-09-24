c47946b20410eff7d6219de4df1ed3e377d93f86
jeremy <jeremypoliquit.dev@gmail.com>
2026-09-24 13:28:37 +0800

    optimized map v2

--- frontend/src/components/MapContainer.tsx
+++ frontend/src/components/MapContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import type { Property, NearbyEstablishmentsMap } from '../types';
